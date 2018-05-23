791f25742845208d8f3432279e59f145ba7234a1
Александр Брюханов <greed@ispsystem.com>
2018-05-23 18:49:58 +0800

    Почему-то параметр, содержащий port различается для scp и ssh!!! Забыл

--- cmake_misc/ssh.py
+++ cmake_misc/ssh.py
@@ -8,21 +8,22 @@ def get_ssh_shell(server):
     sys.stderr.write("Build on server: %s\n" % server)
     port = server.rfind(':')
     if port is None:
-        return Ssh(host=server)
+        return Ssh(host=server, port='22')
     else:
-        return Ssh(host=server[0:port], params='ssh -p ' + server[port + 1:])
+        return Ssh(host=server[0:port], port=server[port + 1:])
 
 
 class Ssh(Remote):
-    def __init__(self, host, params=''):
-        super().__init__(host, params)
+    def __init__(self, host, port):
+        super().__init__(host, 'ssh -p ' + port)
+        self._port = port
 
     def sync_artifacts(self, path):
         sys.stderr.write("Get artifacts: %s\n" % path)
         if path[-1] == '/':
             subprocess.call(['rsync', '-a', '-e', self._params, self._host + ':' + path, path[0:-1]])
         else:
-            subprocess.call(['scp'] + self._prepare_args([self._host + ':' + path, path])[1:])
+            subprocess.call(['scp', '-P', self._port, self._host + ':' + path, path])
 
     def call(self, args, desc=None, **kwargs):
         self.desc(args, desc)
